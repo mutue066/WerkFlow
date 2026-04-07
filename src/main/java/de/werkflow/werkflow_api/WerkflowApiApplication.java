af4cd74753d171c7f81fc20ae71a1933b16eae2b
mutue066 <devstation066@outlook.com>
2026-04-07 17:24:36 +0200

    Initial project setup: Spring Boot 3.5, Java 21, Maven

---
+++ src/main/java/de/werkflow/werkflow_api/WerkflowApiApplication.java
@@ -0,0 +1,13 @@
+package de.werkflow.werkflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerkflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerkflowApiApplication.class, args);
+	}
+
+}
